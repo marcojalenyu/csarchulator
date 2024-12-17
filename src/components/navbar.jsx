1d31770d480916999af059e13c9a219aa9a4c1a7
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-12-17 21:35:52 +0800

    feat: Added dark mode at Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,19 +2,39 @@
 import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import './styles.css'
-import Logo from "./logo";
+import Logo from './logo';
 
 const Navbar = () => {
+
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const initialTheme = prefersDarkScheme ? 'dark' : 'light';
+  const [theme, setTheme] = useState(initialTheme);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <nav className="navbar w-100">
-      <div className="nav-title d-inline-flex justify-content-center align-items-center">
+    <nav className="navbar my-1 w-100">
+
+      <a className="fs-5 fw-bold d-inline-flex align-items-center" href="/">
         <Logo />
         <div className="ms-1 font-we">CSARCHulator</div>
-      </div>
+      </a>
       
-      <div className="links">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+      <div className="d-flex flex-column align-items-end">
+        <div className="form-check form-switch mb-1">
+          <input 
+            className="form-check-input mx-0" 
+            type="checkbox" 
+            id="flexSwitchCheckDefault" 
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            checked={theme === 'dark'}
+          />
+        </div>
+        <div className="links">
+          <a className="fw-bold" href="/">Choose a converter... </a>
+        </div>
       </div>
     </nav>
   );
