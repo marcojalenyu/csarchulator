0ac58ef9b6f805de2e7b879250d9741df653561b
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-12-17 21:35:12 +0800

    ui: Updated logo component to handle versions

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,13 +1,36 @@
 "use client";
-import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
-import logo from '../logo.svg';
+import logo1 from '../assets/logo1.svg';
+import logo2 from '../assets/logo2.svg';
+import logo3 from '../assets/logo3.svg';
+import logo4 from '../assets/logo4.svg';
 
-const Logo = () => {
+
+const Logo = ({ width = 50, height = 50, type = 1 }) => {
+
+    var logo;
+
+    switch (type) {
+        case 1:
+            logo = logo1;
+            break;
+        case 2:
+            logo = logo2;
+            break;
+        case 3:
+            logo = logo3;
+            break;
+        case 4:
+            logo = logo4;
+            break;
+        default:
+            logo = logo2;
+    }
+    
     return (
-        <div className="logo d-inline">
-            <img src={logo} alt="logo" width={50} height={50}/>
+        <div className="d-inline">
+            <img className="logo-img" src={logo} alt="logo" width={width} height={height}/>
         </div>
     );
 }
